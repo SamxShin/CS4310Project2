ac4cc6df6513ea1a48b9bffd0945b01f437831ee
SamxShin <47575974+SamxShin@users.noreply.github.com>
2021-05-09 19:21:18 -0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args){
+        String[] fileContent;
+
+        int totalTestCases = 50;
+
+        double fifoFaultCount = 0;
+        double lruFaultCount = 0;
+        double oaFaultCount = 0;
+
+        FileGenerator fileGenerator = new FileGenerator();
+
+    }
 }
